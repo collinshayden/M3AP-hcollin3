ece9dece4438bfdd11396fd72a7e8de1454478a6
collinshayden <collinshayden171@gmail.com>
2023-03-26 15:10:02 -0400

    finished python script

--- graph.py
+++ graph.py
@@ -1,22 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open('data/bubble.txt', 'r') as bubble:
-    data = bubble.readlines()
-bubble.close()
-for i in range(len(data)):
-    data[i] = data[i].rstrip().split(',')
-    data[i] = list(map(int, data[i]))
 
-reads = []
-writes = []
-for i in data:
-    reads.append(i[0])
-    writes.append(i[1])
+sort_types = ['bubble', 'heap', 'selection', 'quick', 'twosort']
 
-print(reads)
-print(writes)
-x = np.linspace(0, 1000, 10)
+for sort_type in sort_types:
+    with open('data/' + sort_type + '.txt', 'r') as file:
+        data = file.readlines()
+    file.close()
 
-plt.plot(x, reads, x, writes)
-plt.show()
+    for i in range(len(data)):
+        data[i] = data[i].rstrip().split(',')
+        data[i] = list(map(int, data[i]))
+
+    reads = []
+    writes = []
+    for i in data:
+        reads.append(i[0])
+        writes.append(i[1])
+
+    x = np.linspace(0, 1000, 10)
+
+    plt.plot(x, reads, label='reads')
+    plt.plot(x, writes, label='writes')
+    plt.legend(loc="upper left")
+    plt.title(sort_type + ' sort read and writes')
+    plt.ylabel('# of read/writes')
+    plt.xlabel('input size')
+    plt.savefig('images/' + sort_type + '.jpeg')
+    plt.show()
+    plt.close()
